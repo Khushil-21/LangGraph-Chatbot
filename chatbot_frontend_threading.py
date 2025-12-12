9433435446f88ba14c329766029bb1a579f5eda7
Khushil-21 <khushilshah2105@gmail.com>
2025-12-13 04:06:28 +0530

    Multiple chats

--- chatbot_frontend_threading.py
+++ chatbot_frontend_threading.py
@@ -3,25 +3,39 @@ from langchain_core.messages import HumanMessage
 from chatbot_backend import chat_bot
 from utils import get_thread_id
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
-
-if "thread_id" not in st.session_state:
-    st.session_state["thread_id"] = [get_thread_id()]
-
-CONFIG = {"configurable": {"thread_id": st.session_state["thread_id"]}}
+if "chat_history" not in st.session_state:
+    thread_id = get_thread_id()
+    current_thread_id = thread_id
+    st.session_state["chat_history"] = {thread_id: {"messages": []}}
+    st.session_state["current_thread_id"] = thread_id
 
 
-# st.title(f"Chatbot - Thread ID: {st.session_state['thread_id'][0]}")
+def new_chat():
+    new_thread_id = get_thread_id()
+    st.session_state["current_thread_id"] = new_thread_id
+    st.session_state["chat_history"][new_thread_id] = {"messages": []}
+
+
+def switch_thread(thread_id):
+    st.session_state["current_thread_id"] = thread_id
+
+
+CONFIG = {"configurable": {"thread_id": st.session_state["current_thread_id"]}}
+
+st.title(f"Chatbot - Thread ID: {st.session_state['current_thread_id']}")
 
 st.sidebar.title(f"LangGraph Chatbot")
-st.sidebar.button("New Chat")
+st.sidebar.button("New Chat", on_click=new_chat)
 st.sidebar.header("Past Chats")
-for thread_id in st.session_state["thread_id"]:
-    st.sidebar.button(f"Thread ID: {thread_id}")
+for thread_id in st.session_state["chat_history"]:
+    st.sidebar.button(
+        f"Thread ID: {thread_id}", on_click=switch_thread, args=(thread_id,)
+    )
 
 
-for message in st.session_state["messages"]:
+for message in st.session_state["chat_history"][st.session_state["current_thread_id"]][
+    "messages"
+]:
     with st.chat_message(message["role"]):
         st.text(message["content"])
 
@@ -29,7 +43,9 @@ user_input = st.chat_input("Enter a message:")
 
 if user_input:
     # store the user input in the session state
-    st.session_state["messages"].append({"role": "user", "content": user_input})
+    st.session_state["chat_history"][st.session_state["current_thread_id"]][
+        "messages"
+    ].append({"role": "user", "content": user_input})
     with st.chat_message("user"):
         st.text(user_input)
 
@@ -42,6 +58,6 @@ if user_input:
                 stream_mode="messages",
             )
         )
-        st.session_state["messages"].append(
-            {"role": "assistant", "content": assistant_response}
-        )
+        st.session_state["chat_history"][st.session_state["current_thread_id"]][
+            "messages"
+        ].append({"role": "assistant", "content": assistant_response})
