2a5a3e318cbd2ff30c6daf588affb81bbdda1f99
Khushil-21 <khushilshah2105@gmail.com>
2025-12-12 14:23:12 +0530

    Backend Integration

--- chatbot_frontend.py
+++ chatbot_frontend.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from langchain_core.messages import HumanMessage
+from chatbot_backend import chat_bot
+
+CONFIG = {"configurable": {"thread_id": "1"}}
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
@@ -16,7 +20,9 @@ if user_input:
     with st.chat_message("user"):
         st.text(user_input)
 
+    response = chat_bot.invoke({"messages": [HumanMessage(content=user_input)]}, config=CONFIG)
+    assistant_response = response["messages"][-1].content
     # store the assistant response in the session state
-    st.session_state["messages"].append({"role": "assistant", "content": user_input})
+    st.session_state["messages"].append({"role": "assistant", "content": assistant_response})
     with st.chat_message("assistant"):
-        st.text(user_input)
+        st.text(assistant_response)
